9acf1653926fe4d2eb6924bfc7b79d83c01db923
Fraser Crumpler <fraser@crumpler.id.au>
2025-10-12 11:06:49 +1100

    init rust. start parse args

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+
+    #[arg(short, long)]
+    fetch_data : bool,
+
+    #[arg(short, long)]
+    rebuild_db : bool,
+}
+
+
+fn main() {
+    let args = Args::parse();
+
+    if args.fetch_data { fetch_data(); }
+    if args.rebuild_db { rebuild_db(); }
+
+
+}
+
+
+fn fetch_data() {
+
+}
+
+fn rebuild_db() {
+
+}
