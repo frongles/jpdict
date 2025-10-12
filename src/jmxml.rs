5fc50481214ef53ee9188b4d9845e4f76f3b8685
Fraser Crumpler <fraser@crumpler.id.au>
2025-10-13 09:54:01 +1100

    database setup functions. Early parsing funcitons for JMdict

--- src/jmxml.rs
+++ src/jmxml.rs
@@ -1,7 +1,13 @@
+#![allow(dead_code)]
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use std::collections::HashMap;
+
 
 use serde::Deserialize;
 use serde_xml_rs::from_reader;
 
+const CLEAN_JMDICT : &str = "JMdictCLEAN";
 
 
 #[derive(Debug, Deserialize)]
@@ -22,11 +28,61 @@ struct Reading {
 }
 
 #[derive(Debug, Deserialize)]
-struct Sense;
+struct Sense {
+    pos     : String,
+    x_ref   : Option<String>,
+    gloss   : String,
+}
 
 
 
 fn parse(file : std::fs::File) {
     let dict : JMdict = from_reader(file).unwrap();
+    
+}
 
+/// ---------------------------------------------------------
+/// Preprocess JMDict file, expanding XML entities as their 
+/// full string
+/// ---------------------------------------------------------
+fn preprocess(file : File) {
+    let entities : HashMap<&str, &str> = [
+        ("unc", "Unclassified"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let reader = BufReader::new(file);
+
+    let mut outputfile = File::create(CLEAN_JMDICT).unwrap();
+
+    for line in reader.lines() {
+        let mut line = line.unwrap();
+        for (entity, replacement) in &entities {
+            let pattern = format!("&{}", entity);
+            line = line.replace(&pattern, replacement);
+        }
+        writeln!(outputfile, "{}", line).unwrap();
+    }
+
+}
+
+
+
+
+
+
+/// ------------------------------------------------------------
+/// Test
+/// ------------------------------------------------------------
+
+#[test]
+fn test_parse() {
+    let file = std::fs::File::open("JMdictTest").unwrap();
+    preprocess(file);
+    let file = File::open(CLEAN_JMDICT).unwrap();
+
+    let dict : JMdict = from_reader(file).unwrap();
+    println!("{:?}", dict);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use std::fs;
 use clap::Parser;
+use sqlx::sqlite;
+use reqwest::blocking::get;
+use tokio;
+
 mod jmxml;
+mod sql;
 
-
+const DICT_SERVER   : &str = "http://ftp.edrdg.org/pub/Nihongo/JMdict_b.gz";
+const TARGET_FILE   : &str = "JMdict_b.gz";
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -13,18 +20,55 @@ struct Args {
     rebuild_db : bool,
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
-    //if args.fetch_data { fetch_data(); }
-    if args.rebuild_db { rebuild_db(); }
+    if args.fetch_data { fetch_data(); }
+    if args.rebuild_db { 
+        rebuild_db().await;
+    }
+    
+    let pool = sqlite::SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(sql::DB_FILE)
+        .await
+        .unwrap();
+
 
 
 }
 
 
-
-fn rebuild_db() {
+fn fetch_data () {
+    match fs::remove_file(TARGET_FILE) {
+        Ok(()) => eprintln!("Removed {}", TARGET_FILE),
+        Err(e) => eprintln!("Failed to remove file {}: {}", TARGET_FILE, e),
+    }
+    let mut resp = get(DICT_SERVER).unwrap();
+    let mut out = fs::File::create(TARGET_FILE).unwrap();
+    std::io::copy(&mut resp, &mut out).unwrap();
 
 }
+
+async fn rebuild_db () {
+
+    sql::rebuild_db().await.unwrap();
+
+}
+
+
+
+
+
+/// ------------------------------------------------------------------------
+/// Tests
+/// ------------------------------------------------------------------------
+
+#[test]
+#[ignore]
+fn test_fetch_data() {
+
+    fetch_data();
+
+} 
